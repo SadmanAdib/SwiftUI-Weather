48b9abcd3a2e764d0ff32ec4e54b67e10f817d41
Sadman Adib <sadmanadib67@gmail.com>
2022-07-17 16:25:32 +0600

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Sadman Adib on 17/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
