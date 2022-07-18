7c61321eadb33c3db6d005c54c5e783988a22bfa
Sadman Adib <sadmanadib67@gmail.com>
2022-07-18 11:48:25 +0600

    @Binding to bind two variables

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-            BackgroundView(topColor: isNight ? .black : .blue, bottomColor: isNight ? .gray : Color("lightBlue"))
+            BackgroundView(isNight: $isNight)
             
             VStack {
                 CityTextView(cityName: "Dhaka")
@@ -56,11 +56,10 @@ struct ContentView_Previews: PreviewProvider {
 
 struct BackgroundView: View {
     
-    var topColor: Color
-    var bottomColor: Color
+   @Binding var isNight: Bool
     
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing)
             .edgesIgnoringSafeArea(.all)
     }
 }
